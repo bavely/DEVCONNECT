8d6c3417b518a9dd623ddc2a264099ef1fca24a5
Home <Home@Homes-MacBook-Air.local>
2017-07-14 21:39:35 -0700

    7-14 9:40

---
+++ public/javascript/socket.js
@@ -0,0 +1,46 @@
+// module.exports = function() {
+//   var someVal = $('#userEmail').html();
+// };
+
+      $(function () {
+
+        var socket = io();
+        var val=$('#userEmail').html();
+
+        $('#signUp').on("click", function(event){
+          event.preventDefault();
+          socket.emit('chat message', $('#chatmessage').val());
+          $('#chatmessage').val('');
+          return false;
+        });
+
+        $('#directsend').on("click", function(event){
+          event.preventDefault();
+          socket.emit('direct message', $('#directmessage').val() + "^" + $('#mssgsearch').val());
+          $('#directmessage').val('');
+          return false;
+        });
+
+
+        // console.log('this is user email ' + $('#userEmail').contents());
+
+
+        $(document).ready(function(){
+          console.log(val);
+          socket.emit('join', val);
+          $('#email').val('');
+          return false;
+        });
+   
+
+        socket.on('chat message', function(msg){
+          $('#chatlist').append($('<li>').text(msg));
+          window.scrollTo(0, document.body.scrollHeight);
+        });
+
+        socket.on('direct message', function(msg){
+          $('#mssgfield').append($('<li>').text(msg));
+          window.scrollTo(0, document.body.scrollHeight);
+        });
+
+  });
